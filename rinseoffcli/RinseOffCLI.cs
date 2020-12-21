cce0989c087d777d77a4bd7ede20e901e66b7c99
Kevin Froman <beardog@mailbox.org>
2020-12-21 08:14:27 +0000

    added license and readme

--- rinseoffcli/RinseOffCLI.cs
+++ rinseoffcli/RinseOffCLI.cs
@@ -2,6 +2,24 @@
 using System.IO;
 using System.Collections.Generic;
 using rinseoff;
+/*
+    RinseOff - Encrypt or load data with a key file to help with secure erasure
+    Copyright (C) <2020>  Kevin Froman
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 namespace rinseoffcli
 {
     class Program
@@ -9,6 +27,7 @@ namespace rinseoffcli
         public static string version = "0.0.0";
         enum ErrorCode : byte
         {
+            // Exit error codes indexed from 1
             InvaildArgs = 1,
             NoDataFileFound,
             NoPermissionToReadDataFile,
